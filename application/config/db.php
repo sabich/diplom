19c1f41a638b5785b85fa7e81d683e70409d1ca4
George Lemish <yorgus@yandex.ru>
2017-04-13 22:22:48 +0300

    configure database connection

--- application/config/db.php
+++ application/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2-piramida',
+    'dsn' => 'mysql:host=localhost;dbname=yii2-borisov',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
