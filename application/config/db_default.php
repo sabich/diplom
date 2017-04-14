02c7879c99bba12d3ea6d2222a49dfb5589e87e8
Borisov Sergey <borisov_s@ukr.net>
2017-04-14 20:03:02 +0300

    ignore db config

---
+++ application/config/db_default.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost:3306;dbname=yii2-borisov',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
