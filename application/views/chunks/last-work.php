e44c0a1e7f0599df54d11fdf7265e9a440a7fc9d
Borisov Sergey <borisov_s@ukr.net>
2017-05-15 23:43:57 +0300

    fix style index page

--- application/views/chunks/last-work.php
+++ application/views/chunks/last-work.php
@@ -9,7 +9,7 @@ use yii\helpers\Url;
 ?>
 
 <!-- LAST WORKS -->
-<section id="last_works">
+<section id="last_works" class="container-fluid">
     <div class="row">
         <div class="col-md-12 text-center">
             <h2 class="title_main">Последние проекты</h2>
