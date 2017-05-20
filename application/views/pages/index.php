24b5cb34863c10f0bc0dabb6c1484255f463d79e
Borisov Sergey <borisov_s@ukr.net>
2017-05-20 08:09:53 +0300

    change main slider

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -10,21 +10,21 @@ $this->title = 'ARS Holding';
 <section id="main-slider">
     <div id="main_slider" class="owl-carousel owl-theme">
         <div class="item">
-            <img src="/images/slider/image01.jpg" alt="">
+            <img src="/images/slider/image06.jpg" alt="">
             <div class="item-block">
                 <div class="slide-caption">
                     <img class="logo-max" src="/images/logo/logo-max.png" alt="АРС-Холдинг" width="122" height="147">
                     <span class="red-line"></span>
-                    <h2>Учереждения здравохранения</h2>
+                    <h2>Индивидуальное жилье</h2>
                 </div>
                 <div class="slide-text">
-                    <p>Проект, гармонично сочетающий в себе строгие технические и медицинские регламенты с высокой комфортабельностью лечебных помещений.</p>
+                    <p>Дом, ставший органичным дополнением окружающего ландшафта – таков архитектурный концепт двухэтажного коттеджа с просторной мансардой и окнами-витражами.</p>
                 </div>
-                <div class="nav_slider text left">
-                    <span class="btn customPrevBtn"><i class="fa fa-long-arrow-left fa-2x" aria-hidden="true"></i></span>
-                    <span>|</span>
-                    <span class="btn customNextBtn"><i class="fa fa-long-arrow-right fa-2x" aria-hidden="true"></i></span>
-                </div>
+<!--                <div class="nav_slider text left">-->
+<!--                    <span class="btn customPrevBtn"><i class="fa fa-long-arrow-left fa-2x" aria-hidden="true"></i></span>-->
+<!--                    <span>|</span>-->
+<!--                    <span class="btn customNextBtn"><i class="fa fa-long-arrow-right fa-2x" aria-hidden="true"></i></span>-->
+<!--                </div>-->
                 <a href="#order" class="btn-order">Заказать проект</a>
             </div>
         </div>
@@ -39,30 +39,11 @@ $this->title = 'ARS Holding';
                 <div class="slide-text">
                     <p>Утонченный интерьер частных апартаментов спроектирован с учетом всех правил эргономики жилых пространств.</p>
                 </div>
-                <div class="nav_slider text left">
-                    <span class="btn customPrevBtn"><i class="fa fa-long-arrow-left fa-2x" aria-hidden="true"></i></span>
-                    <span>|</span>
-                    <span class="btn customNextBtn"><i class="fa fa-long-arrow-right fa-2x" aria-hidden="true"></i></span>
-                </div>
-                <a href="#order" class="btn-order">Заказать проект</a>
-            </div>
-        </div>
-        <div class="item">
-            <img src="/images/slider/image03.jpg" alt="">
-            <div class="item-block">
-                <div class="slide-caption">
-                    <img class="logo-max" src="/images/logo/logo-max.png" alt="АРС-Холдинг" width="122" height="147">
-                    <span class="red-line"></span>
-                    <h2>Ландшафтный дизайн</h2>
-                </div>
-                <div class="slide-text">
-                    <p>Благоустройство существующей парковой территории и проектирование новых спортивных и этно-культурных зон вносит огромный вклад в улучшение городской инфраструктуры.</p>
-                </div>
-                <div class="nav_slider text left">
-                    <span class="btn customPrevBtn"><i class="fa fa-long-arrow-left fa-2x" aria-hidden="true"></i></span>
-                    <span>|</span>
-                    <span class="btn customNextBtn"><i class="fa fa-long-arrow-right fa-2x" aria-hidden="true"></i></span>
-                </div>
+<!--                <div class="nav_slider text left">-->
+<!--                    <span class="btn customPrevBtn"><i class="fa fa-long-arrow-left fa-2x" aria-hidden="true"></i></span>-->
+<!--                    <span>|</span>-->
+<!--                    <span class="btn customNextBtn"><i class="fa fa-long-arrow-right fa-2x" aria-hidden="true"></i></span>-->
+<!--                </div>-->
                 <a href="#order" class="btn-order">Заказать проект</a>
             </div>
         </div>
@@ -77,11 +58,30 @@ $this->title = 'ARS Holding';
                 <div class="slide-text">
                     <p>Автосалон с просторными торгово-выставочными залами и собственной СТО спроектирован с учетом корпоративных стандартов и имиджа дилерской компании.</p>
                 </div>
-                <div class="nav_slider text left">
-                    <span class="btn customPrevBtn"><i class="fa fa-long-arrow-left fa-2x" aria-hidden="true"></i></span>
-                    <span>|</span>
-                    <span class="btn customNextBtn"><i class="fa fa-long-arrow-right fa-2x" aria-hidden="true"></i></span>
+<!--                <div class="nav_slider text left">-->
+<!--                    <span class="btn customPrevBtn"><i class="fa fa-long-arrow-left fa-2x" aria-hidden="true"></i></span>-->
+<!--                    <span>|</span>-->
+<!--                    <span class="btn customNextBtn"><i class="fa fa-long-arrow-right fa-2x" aria-hidden="true"></i></span>-->
+<!--                </div>-->
+                <a href="#order" class="btn-order">Заказать проект</a>
+            </div>
+        </div>
+        <div class="item">
+            <img src="/images/slider/image03.jpg" alt="">
+            <div class="item-block">
+                <div class="slide-caption">
+                    <img class="logo-max" src="/images/logo/logo-max.png" alt="АРС-Холдинг" width="122" height="147">
+                    <span class="red-line"></span>
+                    <h2>Ландшафтный дизайн</h2>
                 </div>
+                <div class="slide-text">
+                    <p>Благоустройство существующей парковой территории и проектирование новых спортивных и этно-культурных зон вносит огромный вклад в улучшение городской инфраструктуры.</p>
+                </div>
+                <!--                <div class="nav_slider text left">-->
+                <!--                    <span class="btn customPrevBtn"><i class="fa fa-long-arrow-left fa-2x" aria-hidden="true"></i></span>-->
+                <!--                    <span>|</span>-->
+                <!--                    <span class="btn customNextBtn"><i class="fa fa-long-arrow-right fa-2x" aria-hidden="true"></i></span>-->
+                <!--                </div>-->
                 <a href="#order" class="btn-order">Заказать проект</a>
             </div>
         </div>
@@ -96,30 +96,30 @@ $this->title = 'ARS Holding';
                 <div class="slide-text">
                     <p>Спортивный объект с пристроенным административным комплексом спроектирован на 660 зрительских мест и предполагает специальное пространство для посетителей с ограниченными возможностями.</p>
                 </div>
-                <div class="nav_slider text left">
-                    <span class="btn customPrevBtn"><i class="fa fa-long-arrow-left fa-2x" aria-hidden="true"></i></span>
-                    <span>|</span>
-                    <span class="btn customNextBtn"><i class="fa fa-long-arrow-right fa-2x" aria-hidden="true"></i></span>
-                </div>
+<!--                <div class="nav_slider text left">-->
+<!--                    <span class="btn customPrevBtn"><i class="fa fa-long-arrow-left fa-2x" aria-hidden="true"></i></span>-->
+<!--                    <span>|</span>-->
+<!--                    <span class="btn customNextBtn"><i class="fa fa-long-arrow-right fa-2x" aria-hidden="true"></i></span>-->
+<!--                </div>-->
                 <a href="#order" class="btn-order">Заказать проект</a>
             </div>
         </div>
         <div class="item">
-            <img src="/images/slider/image06.jpg" alt="">
+            <img src="/images/slider/image01.jpg" alt="">
             <div class="item-block">
                 <div class="slide-caption">
                     <img class="logo-max" src="/images/logo/logo-max.png" alt="АРС-Холдинг" width="122" height="147">
                     <span class="red-line"></span>
-                    <h2>Индивидуальное жилье</h2>
+                    <h2>Учереждения здравохранения</h2>
                 </div>
                 <div class="slide-text">
-                    <p>Дом, ставший органичным дополнением окружающего ландшафта – таков архитектурный концепт двухэтажного коттеджа с просторной мансардой и окнами-витражами.</p>
+                    <p>Проект, гармонично сочетающий в себе строгие технические и медицинские регламенты с высокой комфортабельностью лечебных помещений.</p>
                 </div>
-                <div class="nav_slider text left">
-                    <span class="btn customPrevBtn"><i class="fa fa-long-arrow-left fa-2x" aria-hidden="true"></i></span>
-                    <span>|</span>
-                    <span class="btn customNextBtn"><i class="fa fa-long-arrow-right fa-2x" aria-hidden="true"></i></span>
-                </div>
+                <!--                <div class="nav_slider text left">-->
+                <!--                    <span class="btn customPrevBtn"><i class="fa fa-long-arrow-left fa-2x" aria-hidden="true"></i></span>-->
+                <!--                    <span>|</span>-->
+                <!--                    <span class="btn customNextBtn"><i class="fa fa-long-arrow-right fa-2x" aria-hidden="true"></i></span>-->
+                <!--                </div>-->
                 <a href="#order" class="btn-order">Заказать проект</a>
             </div>
         </div>
@@ -129,7 +129,7 @@ $this->title = 'ARS Holding';
 <section id="main_menu" class="container-fluid">
     <ul class="nav">
         <li class="col-md-3">
-            <a href="#" class="no-padding" id="btnModal" data-target=".bd-modal-lg">
+            <a href="<?= Url::to('catalog/projects') ?>">
                 <div id="m_projects" class="item_menu"><h2>Проектирование</h2></div>
             </a>
         </li>
@@ -149,63 +149,4 @@ $this->title = 'ARS Holding';
             </a>
         </li>
     </ul>
-</section>
-<section>
-    <div class="modal fade bd-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Проектирование</h4>
-                </div>
-                <div class="modal-body">
-                <div class="row">
-
-                    <div class="col-md-6"><div id="wheelDiv"></div></div>
-
-                    <div class="col-md-6">
-                        <div class="tab-content">
-                            <ul class="nav nav-tabs" id="tabNav" hidden="hidden" >
-                                <li class=""><a href="#tabHome" data-toggle="tab">Индивидуальное жилье</a></li>
-                                <li class=""><a href="#tabKom" data-toggle="tab">Жилые комплексы</a></li>
-                                <li class=""><a href="#tabProm" data-toggle="tab">Промышленные объекты</a></li>
-                                <li class=""><a href="#tabRaz" data-toggle="tab">Развлекательные комплексы</a></li>
-                            </ul>
-
-                            <div class="tab-pane fade in active" id="tabHome">
-                                <h2 class="featurette-heading">
-                                    Индивидуальное жилье
-                                </h2>
-                                <h3 class="text-muted">Скромное загородное шале или фешенебельный городской коттедж - любой жилой объект, доверенный инженерам ARS Holding, будет детально проработан согласно строгим нормативам строительства и с учетом идей и потребностей заказчика.</h3>
-                                <p class="lead">
-                                    <a href="<?= Url::toRoute(['catalog/projects', 'typeId' => '1']) ?>" class="btn btn-lg btn-default btn-link">Перейти в каталог проектов</a></p>
-                            </div>
-                            <div class="tab-pane fade" id="tabKom">
-                                <h2 class="featurette-heading">
-                                    Жилые комплексы
-                                </h2>
-                                <h3 class="text-muted">Проектная деятельность холдинга в сфере строительства многоквартирного жилья базируется на строжайшем соблюдении нормативов и применении новых для Казахстана антисейсмических технологий.</h3>
-                                <p class="lead"><a href="<?= Url::toRoute(['catalog/projects', 'typeId' => '2']) ?>" class="btn btn-lg btn-default btn-link">Перейти в каталог проектов</a></p>
-                            </div>
-                            <div class="tab-pane fade" id="tabProm">
-                                <h2 class="featurette-heading">
-                                    Промышленные объекты
-                                </h2>
-                                <h3 class="text-muted">Промышленное проектирование включает в себя работу над различными по масштабам и функциональному назначению проектами – от небольших заправочных станций до заводов и аэропортов.</h3>
-                                <p class="lead"><a href="<?= Url::toRoute(['catalog/projects', 'typeId' => '3']) ?>" class="btn btn-lg btn-default btn-link">Перейти в каталог проектов</a></p>
-                            </div>
-                            <div class="tab-pane fade" id="tabRaz">
-                                <h2 class="featurette-heading">
-                                    Развлекательные комплексы
-                                    <h3 class="text-muted">Проектирование торговых и развлекательных комплексов требует не только профессионального инженерного просчета, но и проработки таких важных компонентов, как архитектурная уникальность здания, уют и функциональность внутренних помещений, безопасность объекта и удобство его расположения.</h3>
-                                </h2>
-                                <p class="lead"><a href="<?= Url::toRoute(['catalog/projects', 'typeId' => '4']) ?>" class="btn btn-lg btn-default btn-link">Перейти в каталог проектов</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </section>
